10c64cfbd467a4a83dba26b0741d843b54f24d33
webvuln <66708163+webvuln@users.noreply.github.com>
2022-01-27 08:30:16 -0500

    Update proxyscraper.py

--- proxyscraper.py
+++ proxyscraper.py
@@ -1,7 +1,15 @@
-import requests
+#Python3 
+#This tool is made by https://github.com/webvuln 
 
-T = input("HTTP, SOCK4, SOCK5: ")
-r = requests.get(f"https://api.proxyscrape.com/v2/?request=displayproxies&protocol={T}&timeout=10000&country=all&ssl=all&anonymity=all")
-f = open(T + ".txt", "w+")
-f.write(r.text)
-f.close()
+import requests
+import time 
+
+def main():
+  T = input("HTTP, SOCK4, SOCK5: ")
+  print("Fetching your proxys")
+  r = requests.get(f"https://api.proxyscrape.com/v2/?request=displayproxies&protocol={T}&timeout=10000&country=all&ssl=all&anonymity=all")
+  f = open(T + ".txt", "w+")
+  f.write(r.text)
+  f.close()
+  print("Done getting your proxys check the .txt out")
+  time.sleep(5.5)
